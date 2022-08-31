2bdd2f2fe53adf9ad732031da5c5decadcae3a28
Daniël van Drunen <daniel@pixely.me>
2022-08-31 18:08:07 +0200

    Add ID to outlet for articles

--- app/Actions/GenerateArticleModelAction.php
+++ app/Actions/GenerateArticleModelAction.php
@@ -22,6 +22,20 @@ class GenerateArticleModelAction
             break;
         }
 
+        // Turn ['outlet' => name] into ['outlet' => ['id' => id, 'name' => name]]
+        foreach (config('mock_data.outlets') as $i => $outlet) {
+            if ($data['outlet'] !== $outlet['name']) {
+                continue;
+            }
+
+            $data['outlet'] = [
+                'id' => $i + 1,
+                'name' => $outlet['name'],
+            ];
+
+            break;
+        }
+
         $data['snippet'] = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sem justo, maximus at sagittis quis, fermentum sit amet lectus. Donec tempor mi sed metus dignissim, id condimentum odio rhoncus. Nam sit amet semper erat. Integer a purus euismod, sagittis nunc a, mollis ligula. Sed quis sollicitudin nisl. Vestibulum egestas porta arcu imperdiet elementum. Integer ut rutrum nunc. Curabitur id dui ultrices, dapibus quam ac, feugiat massa.';
 
         return $data;
